54bf571c6993a1aa1a93b798eacf2d5aa907ce23
Veronika-Pav <peepinmyheart13@gmail.com>
2024-12-17 23:57:33 +0700

    first commit

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -26,12 +26,12 @@ const resources = {
   svgSprite: "src/assets/svg-sprite/*.svg",
   static: [
     "src/assets/icons/**/*.*",
-    "src/assets/favicons/**/*.*",
+    //"src/assets/favicons/**/*.*",
     "src/assets/fonts/**/*.{woff,woff2}",
-    "src/assets/video/**/*.{mp4,webm}",
-    "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
-    "src/json/**/*.json",
-    "src/php/**/*.php"
+    //"src/assets/video/**/*.{mp4,webm}",
+    //"src/assets/audio/**/*.{mp3,ogg,wav,aac}",
+    //"src/json/**/*.json",
+    //"src/php/**/*.php"
   ]
 };
 // Gulp Tasks:
@@ -102,17 +102,17 @@ function copy() {
     .pipe(gulp.dest("dist/"));
 }
 function images() {
-  return gulp
-    .src(resources.images)
-    .pipe(
-      imagemin([
-        imagemin_gifsicle({ interlaced: true }),
-        imagemin_mozjpeg({ quality: 100, progressive: true }),
-        imagemin_optipng({ optimizationLevel: 3 })
-      ])
-    )
-    .pipe(gulp.dest("dist/assets/images"));
-}
+    return gulp
+      .src(resources.images, { encoding: false })
+      .pipe(
+        imagemin([
+          imagemin_gifsicle({ interlaced: true }),
+          imagemin_mozjpeg({ quality: 100, progressive: true }),
+          imagemin_optipng({ optimizationLevel: 5 })
+        ])
+      )
+      .pipe(gulp.dest('dist/assets/images'));
+  }
 function svgSprite() {
   return gulp
     .src(resources.svgSprite)
